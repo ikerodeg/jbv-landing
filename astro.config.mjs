a759f15235d2fbbe21f6a4689acc2f64d3fb9ccc
ikerodeg <ocp2k.android@gmail.com>
2026-03-13 10:53:49 +0100

    ci: add GitHub Actions workflow and base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.jbv.com',
+  site: 'https://ikerodeg.github.io',
+  base: '/jbv-landing',
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto',
